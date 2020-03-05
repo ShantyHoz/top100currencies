abb62de13a1e80fedc46f793686a63a95121d088
hoz <progerhoz@gmail.com>
2020-03-05 17:13:09 +0300

    Added layout AboutActivity with resourses for it

---
+++ app/src/main/java/info/fandoid/top100currencies/activities/AboutActivity.kt
@@ -0,0 +1,13 @@
+package info.fandoid.top100currencies.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import info.fandoid.top100currencies.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
