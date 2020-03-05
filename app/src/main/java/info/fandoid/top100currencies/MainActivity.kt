82362717328b7d7805785c254bbe15c7830e9d1b
hoz <progerhoz@gmail.com>
2020-03-05 16:40:34 +0300

    Init project's state

---
+++ app/src/main/java/info/fandoid/top100currencies/MainActivity.kt
@@ -0,0 +1,12 @@
+package info.fandoid.top100currencies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
