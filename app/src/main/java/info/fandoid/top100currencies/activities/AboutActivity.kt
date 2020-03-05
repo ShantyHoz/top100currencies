9a5b8eae51ee3af46817f47d292dc5f82510cbf8
hoz <progerhoz@gmail.com>
2020-03-05 17:19:33 +0300

    It AboutActivity added listener to open this app's page

--- app/src/main/java/info/fandoid/top100currencies/activities/AboutActivity.kt
+++ app/src/main/java/info/fandoid/top100currencies/activities/AboutActivity.kt
@@ -1,13 +1,20 @@
 package info.fandoid.top100currencies.activities
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import info.fandoid.top100currencies.R
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        buttonRateApp.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
+        }
     }
 }
