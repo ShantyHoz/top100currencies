902a4215be33efa2c9728af124d0bb26fc25bb73
hoz <progerhoz@gmail.com>
2020-03-13 16:13:08 +0300

    Added banners

--- app/src/main/java/info/fandoid/top100currencies/MainActivity.kt
+++ app/src/main/java/info/fandoid/top100currencies/MainActivity.kt
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.InterstitialAd
 import info.fandoid.top100currencies.activities.AboutActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mInterstitialAd: InterstitialAd
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mInterstitialAd = InterstitialAd(this)
+        mInterstitialAd.adUnitId = "ca-app-pub-4542745973299381/8259464248"
+        mInterstitialAd.loadAd(AdRequest.Builder().build())
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -24,8 +32,6 @@ class MainActivity : AppCompatActivity() {
 
         when (item!!.itemId) {
             R.id.action_about -> {
-
-
                 val intent = Intent(this, AboutActivity::class.java)
                 startActivity(intent)
                 return true
@@ -33,5 +39,21 @@ class MainActivity : AppCompatActivity() {
 
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        showAd()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showAd()
+    }
+
+    private fun showAd() {
+        if (mInterstitialAd.isLoaded) {
+            mInterstitialAd.show()
+        }
     }
 }
--- app/src/main/java/info/fandoid/top100currencies/activities/AboutActivity.kt
+++ app/src/main/java/info/fandoid/top100currencies/activities/AboutActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
 import info.fandoid.top100currencies.R
 import kotlinx.android.synthetic.main.activity_about.*
 
@@ -12,6 +13,11 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
 
         buttonRateApp.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=${applicationContext.packageName}")))
